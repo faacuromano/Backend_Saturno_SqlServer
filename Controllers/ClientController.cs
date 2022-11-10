83759b0ff18218565eeea65038b48b940afebaa6
aeloh1m <melowgz@gmail.com>
2022-11-09 23:35:22 -0300

    terminado el enrutado para cada endpoint de los httpmethods

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -6,7 +6,8 @@ namespace Saturno_Backend.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ClientController : ControllerBase {
+public class ClientController : ControllerBase
+{
 
     public readonly ClientService _service;
     public ClientController(ClientService client)
@@ -14,65 +15,66 @@ public class ClientController : ControllerBase {
         _service = client;
     }
 
-    [HttpGet]
+    [HttpGet("getall")]
     public async Task<IEnumerable<Client>> Get()
     {
         return await _service.GetAll();
     }
 
-    [HttpGet("{id}")]
+    [HttpGet("get/{id}")]
     public async Task<ActionResult<Client>> GetById(int id)
     {
         var client = await _service.GetById(id);
-        
-        if(client is null)
+
+        if (client is null)
         {
             return NotFound(
                 new { message = $"El cliente con ID = {id} no existe." });
         }
 
-       return client;
+        return client;
     }
 
-    [HttpPost]
+    [HttpPost("create")]
     public async Task<IActionResult> Create(Client client)
     {
         var newClient = await _service.Create(client);
 
-        return CreatedAtAction(nameof(GetById), new {id = newClient.Id}, newClient);
+        return CreatedAtAction(nameof(GetById), new { id = newClient.Id }, newClient);
     }
 
-    [HttpPut("{id}")]
+    [HttpPut("update/{id}")]
     public async Task<IActionResult> Update(int id, Client client)
     {
         if (id != client.Id)
         {
             return BadRequest(new { message = $"El ID({id}) de la URL no coincide con el ID({client.Id}) de la Body Request." });
         }
-         
-         var clientToUpdate = await _service.GetById(id);
 
-         if(clientToUpdate != null)
-         {
+        var clientToUpdate = await _service.GetById(id);
+
+        if (clientToUpdate != null)
+        {
             await _service.Update(id, client);
             return NoContent();
-         }
-         else{return NotFound(); }
+        }
+        else { return NotFound(); }
     }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
+    [HttpDelete("delete/{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+
+        var clientToDelete = await _service.GetById(id);
+
+        if (clientToDelete != null)
         {
-
-         var clientToDelete = await _service.GetById(id);
-
-         if(clientToDelete != null)
-         {
             await _service.Delete(id);
             return Ok();
 
-         } else{ return NotFound(); } 
-
         }
+        else { return NotFound(); }
+
+    }
 
 }
