47ccadf55eeb24a1c328dffccbf23882761d6d24
FacuRomano <facundo-romano@hotmail.com>
2022-11-10 01:17:16 -0300

    merge admin user

---
+++ Data/Models/Administrator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Saturno_Backend.Data.Models;
+
+public partial class Administrator
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Pasword { get; set; } = null!;
+
+    public string AdminType { get; set; } = null!;
+
+    public DateTime RegDate { get; set; }
+}
--- Data/Models/Turn.cs
+++ Data/Models/Turn.cs
@@ -28,4 +28,3 @@ public partial class Turn
     [JsonIgnore]
     public virtual Service ServiceNavigation { get; set; } = null!;
 }
-    
